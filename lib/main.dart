12b98309d91081f1388e12ceafe38c0f02c7579e
sarmadparvez <sarmadparvez92@gmail.com>
2020-08-18 17:30:22 +0200

    feat(flutter): Add drawer, card and text field with stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -17,40 +17,89 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[200],
       appBar: AppBar(
-        title: Text("Sarmad's App"),
+        title: Text("Sarmad's Appp"),
       ),
-      body: Container(
-        height: 100,
-        color: Colors.teal,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
+      body: Center(
+          child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          child: Card(
+            child: Column(
+              children: <Widget>[
+                Image.asset(
+                  "assets/xps1.jpeg",
+                  //width: 100,
+                  //height: 100,
+                  //fit: BoxFit.fitHeight,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  "Change my name",
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: "Enter text here",
+                      labelText: "My label",
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      )),
+      drawer: Drawer(
+        child: ListView(
+          padding: const EdgeInsets.all(0),
           children: <Widget>[
-            Container(
-              padding: const EdgeInsets.all(8),
-              color: Colors.red,
-              width: 100,
-              height: 100,
-              alignment: Alignment.center,
+            // DrawerHeader(
+            //   child: Text("Drawer Header"),
+            //   decoration: BoxDecoration(
+            //     color: Colors.purple,
+            //   ),
+            // ),
+            UserAccountsDrawerHeader(
+              accountName: Text("Sarmad Parvez"),
+              accountEmail: Text("saramdparvez92@gmail.com"),
+              //   currentAccountPicture: Image.network(
+              //       'https://images.unsplash.com/photo-1527538079466-b6297ad15363?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80'),
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: NetworkImage(
+                    "https://images.unsplash.com/photo-1527538079466-b6297ad15363?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"),
+              ),
             ),
-            Container(
-              padding: const EdgeInsets.all(8),
-              color: Colors.yellow,
-              width: 100,
-              height: 100,
-              alignment: Alignment.center,
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text("Sarmad Parvez"),
+              subtitle: Text("Developer"),
+              trailing: Icon(Icons.edit),
+              onTap: () {},
             ),
-            Container(
-              padding: const EdgeInsets.all(8),
-              color: Colors.green,
-              width: 100,
-              height: 100,
-              alignment: Alignment.center,
+            ListTile(
+              leading: Icon(Icons.email),
+              title: Text("Email"),
+              subtitle: Text("sarmadparvez92@gmail.com"),
+              trailing: Icon(Icons.edit),
             ),
           ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.send),
+      ),
+      //floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
