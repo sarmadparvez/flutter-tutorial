8ea19d4ad65fe54f45e54c75b96d48acf2bcfab6
sarmadparvez <sarmadparvez92@gmail.com>
2020-08-18 17:56:44 +0200

    feat(flutter): Structuring the app code

--- lib/main.dart
+++ lib/main.dart
@@ -1,127 +1,20 @@
 // MaterialApp
+import 'package:awesome_app/pages/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   //runApp(MyApp());
-  runApp(
-    MaterialApp(
+  runApp(MyNewApp());
+}
+
+class MyNewApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: HomePage(),
       theme: ThemeData(
         primarySwatch: Colors.purple,
       ),
-    ),
-  );
-}
-
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  var myText = "Change My Name";
-  TextEditingController _nameController =
-      TextEditingController(); // controller for text field
-
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    // TODO: implement dispose
-  }
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[200],
-      appBar: AppBar(
-        title: Text("Sarmad's Appp"),
-      ),
-      body: Center(
-          child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: SingleChildScrollView(
-          child: Card(
-            child: Column(
-              children: <Widget>[
-                Image.asset(
-                  "assets/xps1.jpeg",
-                  //width: 100,
-                  //height: 100,
-                  //fit: BoxFit.fitHeight,
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Text(
-                  myText,
-                  style: TextStyle(
-                    fontSize: 25,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: TextField(
-                    controller: _nameController,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: "Enter text here",
-                      labelText: "My label",
-                    ),
-                  ),
-                )
-              ],
-            ),
-          ),
-        ),
-      )),
-      drawer: Drawer(
-        child: ListView(
-          padding: const EdgeInsets.all(0),
-          children: <Widget>[
-            // DrawerHeader(
-            //   child: Text("Drawer Header"),
-            //   decoration: BoxDecoration(
-            //     color: Colors.purple,
-            //   ),
-            // ),
-            UserAccountsDrawerHeader(
-              accountName: Text("Sarmad Parvez"),
-              accountEmail: Text("saramdparvez92@gmail.com"),
-              //   currentAccountPicture: Image.network(
-              //       'https://images.unsplash.com/photo-1527538079466-b6297ad15363?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80'),
-              currentAccountPicture: CircleAvatar(
-                backgroundImage: NetworkImage(
-                    "https://images.unsplash.com/photo-1527538079466-b6297ad15363?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"),
-              ),
-            ),
-            ListTile(
-              leading: Icon(Icons.person),
-              title: Text("Sarmad Parvez"),
-              subtitle: Text("Developer"),
-              trailing: Icon(Icons.edit),
-              onTap: () {},
-            ),
-            ListTile(
-              leading: Icon(Icons.email),
-              title: Text("Email"),
-              subtitle: Text("sarmadparvez92@gmail.com"),
-              trailing: Icon(Icons.edit),
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          myText = _nameController.text;
-          setState(() {});
-        },
-        child: Icon(Icons.send),
-      ),
-      //floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
